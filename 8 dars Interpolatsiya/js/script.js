c4b411415bb1dfc26b4778cc5913e0865e5c121a
Toxirjon <tokhiriy87@gmail.com>
2021-11-28 23:17:18 +0500

    8dars yakuni

--- 8 dars Interpolatsiya/js/script.js
+++ 8 dars Interpolatsiya/js/script.js
@@ -1,6 +1,8 @@
 " use strict";
-
+// Interpolatsiya
 const firstName = "Toxirjon";
 const age = 34;
 
 console.log(`Mening ismim ${firstName} va mening yoshim ${age}`);
+
+// demak `` backtick yordamida interpolatsiyani amalga oshiramiz va bunda uzgaruvchilarni ${} ichiga yozamiz
