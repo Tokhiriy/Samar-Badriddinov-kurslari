2d439f920e653f6f50abf672ae0c84742f46722c
Toxirjon <tokhiriy87@gmail.com>
2021-11-28 22:44:50 +0500

    7 dars added

---
+++ 7 dars/js/script.js
@@ -0,0 +1,26 @@
+" use strict ";
+
+//console.log("Hello world");
+
+//alert("Hello world");
+
+// confirm
+/* const userAge = confirm('Siz 18 yoshdamisiz?');
+console.log(userAge); */
+
+// prompt
+
+/* const userName = prompt("Ismiz nima?");
+console.log(userName); */
+
+/* let answers = [];
+answers[0] = prompt("Ismiz?");
+answers[1] = prompt("Familiyangiz");
+answers[2] = prompt("Yoshiz");
+
+console.log(answers); */
+
+const userAge = +prompt('Yoshiz?', "");
+console.log(userAge + 10);
+
+
