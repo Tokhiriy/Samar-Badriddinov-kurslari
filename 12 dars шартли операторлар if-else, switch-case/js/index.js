6265eb6a80b9844e2150474ec519adbdc0f5da0d
Toxirjon <tokhiriy87@gmail.com>
2021-12-01 08:39:51 +0500

    Added 12th dars

---
+++ 12 dars шартли операторлар if-else, switch-case/js/index.js
@@ -0,0 +1,29 @@
+" use strict";
+
+/* const age = 25;
+if (age < 20) {
+  console.log("Kichkina");
+} else if (age == 25){
+  console.log("Yoshlar teng");
+} else{
+  console.log("Standart yosh");
+}
+
+// ternary operatori
+
+const num = 15;
+num ==10 ? console.log("ok") : console.log("Not right"); // 3tali ternary operatori */
+
+// Switch -case
+
+const num = 50;
+
+switch(num){
+  case 20:
+    console.log("Ok");
+    break;
+  case 30:
+    console.log("30");
+    break;
+  default : console.log("Default case");
+}
