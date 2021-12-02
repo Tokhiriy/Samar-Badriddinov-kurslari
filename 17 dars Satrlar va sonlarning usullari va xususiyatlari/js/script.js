930b3126ecb6fb58b030853bb8291f5406f3cdb6
Toxirjon <tokhiriy87@gmail.com>
2021-12-02 22:10:38 +0500

    17 dars added

---
+++ 17 dars Satrlar va sonlarning usullari va xususiyatlari/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//const text = "Hello world";
+
+//console.log(text.length); //11 yani nechta simvoldan iborat ekanligini aniqlaydi
+//console.log(text[2]); // consolega 0,1,2 chi index dagi yani 3chi harf - l chiqadi
+//console.log(text.toLowerCase()); // hello world barcha harflarni kichkina qildi
+//console.log(text.toUpperCase()); // HELLO WORLD barcha harflarni katta qildi
+//console.log(text.indexOf("d")); // 10 чикади сабаби d 10indexda joylshgan
+
+//const animmal = "kiyik";
+//console.log(animmal.indexOf("i")); // consoleга 0,1 яъни 1чикади сабаби 1чи учраган i
+// 1chi indexда турипти. 2чи i ни хисобга олмаяпти!
+
+//console.log(text.slice(6, 11)); // world
+//console.log(text.substring(6)); //world
+//console.log(text.substr(6, 11)); //world
+/* text.length => bu property bo'lsa, text.toLowerCase() => esa method hisoblanadi
+ularning farqi method funksiya kabi () skopka bilan chaqirilsa, property esa skopkasiz
+ ishlatiladi  */
+
+//const arr = [1, 2, 3, 4, 5];
+//console.log(arr.length);
+
+// Number
+//const number = 8.8;
+//console.log(Math.round(number)); // consoleга 9 чикади округленияси
+
+const borderWidth = "12px";
+console.log(parseInt(borderWidth)); // => string to number 12 chiqadi consolega
