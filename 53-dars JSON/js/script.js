e3ff819535529dd9d211969553120f4c33f8f31e
Toxirjon <tokhiriy87@gmail.com>
2022-03-18 21:35:32 +0500

    53-dars

--- 53-dars JSON/js/script.js
+++ 53-dars JSON/js/script.js
@@ -1,7 +1,24 @@
 "use strict";
 
-const car ={
-  name:"mers",
-  color: "black"
+const car = {
+  name: "mers",
+  color: "black",
+  extra: {
+    isBought: false,
+    isAirbag: true,
+  },
 };
 
+//console.log(JSON.stringify(car));
+/* JSON\js\script.js"
+{"name":"mers","color":"black"} */
+
+/* const json = JSON.stringify(car); // JSON formatga o'tkazish
+console.log(json);
+console.log(JSON.parse(json)); // JSONdan qaytarish obyectga */
+
+const clone = JSON.parse(JSON.stringify(car));
+clone.extra.isBought = true;
+
+console.log(car);
+console.log(clone);
